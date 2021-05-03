7f04505370702320a02f78e0104722f3ac5c0233
Harsh Verma <harshverma0362@gmail.com>
2021-05-03 21:08:44 +0530

    implimented static files on project lvl

--- djangodemo/settings.py
+++ djangodemo/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'djangodemo.urls'
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 TEMPLATES = [
     {
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
