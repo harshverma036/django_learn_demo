5530733a45c90274e83c2b1f2d4d72ad3e41e342
Harsh Verma <harshverma0362@gmail.com>
2021-05-31 14:01:49 +0530

    created db and fetch data from db using .all()

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import StudentDetails
 
 def home_page(request):
-    return render(request, 'demoapp/index.html')
+    students = StudentDetails.objects.all()
+    return render(request, 'demoapp/index.html', {'students': students})
