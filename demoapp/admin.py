71c081d1c4517cf7ba40f259c8f50a2bcf29f107
Harsh Verma <harshverma0362@gmail.com>
2021-05-31 15:01:00 +0530

    Model admin class created in admin.py

--- demoapp/admin.py
+++ demoapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import StudentDetails
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'stuEmail')
+
+admin.site.register(StudentDetails, StudentAdmin)
